be363d36419df434c5af0eab0fdbbd03859af3df
Johan Astrom <johan.astrom89@gmail.com>
2020-11-26 20:53:29 +0100

    Uppdaterat readContacts

--- LeopardTigersJavaverktygHT2020/src/main/java/ContactsIO/ContactsIO.java
+++ LeopardTigersJavaverktygHT2020/src/main/java/ContactsIO/ContactsIO.java
@@ -1,17 +1,30 @@
+package ContactsIO;
+
+import java.io.*;
 
 public class ContactsIO {
 
     public void readContacts() throws IOException {
 
-    try(
-    BufferedReader reader = new BufferedReader(new FileReader("ContactBook.txt")))
+        //private List<Contact> contacts = new ArrayList<>();
 
-    }
+        try (BufferedReader reader = new BufferedReader(new FileReader("ContactBook.txt"))) {
+            String input;
+            while ((input=reader.readLine())!=null){
+                String[] records = input.split(" ");
+                String firstName = records[0];
+                String lastName = records[1];
+                String phoneNumber = records[2];
+                Contact contact = new Contact(firstName, lastName, phoneNumber);
+                contacts.add(contact);
+            }
+        }
 
-    public void writeContact(Contact contact) throws IOException {
+   /* public void writeContact(Contact contact) throws IOException {
 
         try (BufferedWriter writer =new BufferedWriter(new FileWriter("ContactBook.txt")))
 
 
+    } */
     }
 }
