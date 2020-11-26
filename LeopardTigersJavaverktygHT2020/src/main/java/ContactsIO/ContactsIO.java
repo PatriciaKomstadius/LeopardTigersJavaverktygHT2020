428c42c135bee497298a9d650702c435e720e923
Johan Astrom <johan.astrom89@gmail.com>
2020-11-26 20:32:07 +0100

    Lagt till metod writeContact i ContactsIO

--- LeopardTigersJavaverktygHT2020/src/main/java/ContactsIO/ContactsIO.java
+++ LeopardTigersJavaverktygHT2020/src/main/java/ContactsIO/ContactsIO.java
@@ -4,8 +4,13 @@ public class ContactsIO {
     public void readContacts() throws IOException {
 
     try(
-    BufferedReader br = new BufferedReader(new FileReader("ContactBook.txt")))
+    BufferedReader reader = new BufferedReader(new FileReader("ContactBook.txt")))
 
+    }
+
+    public void writeContact(Contact contact) throws IOException {
+
+        try (BufferedWriter writer =new BufferedWriter(new FileWriter("ContactBook.txt")))
 
 
     }
