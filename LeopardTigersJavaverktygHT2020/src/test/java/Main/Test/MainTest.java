fed314908adf687003d255ebb8eaa7a64aa0d164
Patricia K <patricia@PatricisMacBook.lan>
2020-11-27 19:45:09 +0100

    testar att merga mina ändringar med projektet

---
+++ LeopardTigersJavaverktygHT2020/src/test/java/Main/Test/MainTest.java
@@ -0,0 +1,20 @@
+package Main.Test;
+
+import Main.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    void testAdd(){
+        System.out.println("Testar att ändra i testet");
+
+    }
+}
