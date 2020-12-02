0f3ccabce8c0e15f43aa761122e05d45f8890e55
alshaDIDIT <youssef.as@gmail.com>
2020-12-02 12:59:13 +0100

    added contact

---
+++ LeopardTigersJavaverktygHT2020/src/test/java/ContactsTest/ContactTest.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+
+public class ContactTest {
+
+    @Test
+    @DisplayName("Get First Name")
+    public void getFirstNameTest() {
+        Contact contact = new Contact("Nika", "Arya", "0734567891");
+
+        assertTrue(contact.getFirstName().equals("Nika"));
+    }
+
+    @Test
+    @DisplayName("Get Last Name")
+    public void getLastNameTest() {
+        Contact contact = new Contact("Nika", "Arya", "0734567891");
+
+        assertTrue(contact.getLastName().equals("Arya"));
+    }
+
+    @Test
+    @DisplayName("Get Number")
+    public void getNumberTest() {
+        Contact contact = new Contact("Nika", "Arya", "0734567890");
+
+        assertTrue(contact.getNumber().equals("0734567890"));
+    }
+
+    @Test
+    @DisplayName("To String")
+    public void toStringTest() {
+        Contact contact = new Contact("Nika", "Arya", "0734567890");
+
+        assertTrue(contact.toString().equals("\n" + contact.getFirstName() +
+                " " + contact.getLastName() + "  |ID|: " + contact.getNumber()));
+
+        assertNotNull(contact.toString());
+    }
+}
