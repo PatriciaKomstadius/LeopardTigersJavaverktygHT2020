8e24e2bde7debe2f76e54b1147ee3471de431f7e
alshaDIDIT <youssef.as@gmail.com>
2020-12-02 12:33:41 +0100

    added ContactManagement

---
+++ LeopardTigersJavaverktygHT2020/src/test/java/ContactsTest/ContactManagementTest.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class ContactManagementTest {
+
+    @Test
+    @DisplayName("Show contacts")
+    public void showContactsTest() {
+        ContactManagement contactManagement = new ContactManagement();
+        Contact contact = new Contact("Jane", "Doe", "0707000070");
+
+        assertAll("contact",
+                () -> assertEquals("Jane", contact.getFirstName()),
+                () -> assertEquals("Doe", contact.getLastName()),
+                () -> assertEquals("0707000070", contact.getNumber()),
+
+                () -> assertNotEquals("Joe", contact.getFirstName()),
+                () -> assertNotEquals("Jenkins", contact.getLastName()),
+                () -> assertNotEquals("0123456789", contact.getNumber()),
+
+                () -> assertNotNull(contact.getFirstName()),
+                () -> assertNotNull(contact.getLastName()),
+                () -> assertNotNull(contact.getNumber())
+                );
+
+
+    }
+
+    @Test
+    @DisplayName("Add contacts")
+    public void addContactTest() {
+        ContactManagement contactManagement = new ContactManagement();
+
+    }
+
+    @Test
+    @DisplayName("Remove contacts")
+    public void removeContactTest() {
+        Contact contact = new Contact("Jane", "Doe", "0707000070");
+
+        assertNull(contact);
+    }
+
+    @Test
+    @DisplayName("Search First Name")
+    public void searchFirstNameTest() {
+        ContactManagement contactManagement = new ContactManagement();
+
+    }
+
+    @Test
+    @DisplayName("Search Last Name")
+    public void searchLastNameTest() {
+        ContactManagement contactManagement = new ContactManagement();
+
+    }
+}
