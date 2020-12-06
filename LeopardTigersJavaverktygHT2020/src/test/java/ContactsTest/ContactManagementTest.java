71e02fcc7236eccfeb6a00d282708e68821c0c13
alshaDIDIT <youssef.as@gmail.com>
2020-12-06 18:17:06 +0100

    fixed some more in ContactManagementTest

--- LeopardTigersJavaverktygHT2020/src/test/java/ContactsTest/ContactManagementTest.java
+++ LeopardTigersJavaverktygHT2020/src/test/java/ContactsTest/ContactManagementTest.java
@@ -22,60 +22,61 @@ public class ContactManagementTest {
         Contact contact2 = new Contact("Joe", "Jenkins", "0123456789");
 
         ContactManagement contactManagement = new ContactManagement();
-        ContactManagement.addContact(contact1);
-        ContactManagement.addContact(contact2);
+        contactManagement.addContact(contact1);
+        contactManagement.addContact(contact2);
 
-        assertEquals(2, contactManagement.showContacts());
+        int foundContacts = contactManagement.showContacts();
+
+        assertEquals(2, foundContacts);
     }
 
     @Test
     @DisplayName("Add contacts")
     public void addContactTest() {
-        Contact contact1 = new Contact("Jane", "Doe", "0707000070");
-        Contact contact2 = new Contact("Joe", "Jenkins", "0123456789");
-        Contact contact3 = new Contact("Youssef", "Al Shadidi", "0734567890");
+        Contact contact3 = new Contact("Jane", "Doe", "0707000070");
+        Contact contact4 = new Contact("Joe", "Jenkins", "0123456789");
+        Contact contact5 = new Contact("Youssef", "Al Shadidi", "0734567890");
 
         ContactManagement contactManagement = new ContactManagement();
-        contactManagement.addContact(contact1);
-        contactManagement.addContact(contact2);
         contactManagement.addContact(contact3);
+        contactManagement.addContact(contact4);
+        contactManagement.addContact(contact5);
 
         List<Contact> addedContacts = new ArrayList<>();
-        addedContacts.add(contact1);
-        addedContacts.add(contact2);
         addedContacts.add(contact3);
+        addedContacts.add(contact4);
+        addedContacts.add(contact5);
 
-        List<Contact> foundContacts = contactManagement.getContacts();
+        int foundContacts = contactManagement.showContacts();
 
-        assertEquals(3, contactManagement.showContacts());
-        assertNotEquals(2, contactManagement.showContacts());
-        assertEquals(foundContacts, addedContacts);
-        assertArrayEquals(foundContacts.toArray(), addedContacts.toArray());
+        assertEquals(3, foundContacts);
+        assertNotEquals(2, foundContacts);
+        assertEquals(foundContacts, addedContacts.size());
     }
 
     @Test
     @DisplayName("Remove contacts")
     public void removeContactTest() {
-        Contact contact1 = new Contact("Jane", "Doe", "0707000070");
-        Contact contact2 = new Contact("Joe", "Jenkins", "0123456789");
-        Contact contact3 = new Contact("Youssef", "Al Shadidi", "0734567890");
+        Contact contact6 = new Contact("Jane", "Doe", "0707000070");
+        Contact contact7 = new Contact("Joe", "Jenkins", "0123456789");
+        Contact contact8 = new Contact("Youssef", "Al Shadidi", "0734567890");
 
         ContactManagement contactManagement = new ContactManagement();
-        contactManagement.addContact(contact1);
-        contactManagement.addContact(contact2);
-        contactManagement.addContact(contact3);
+        contactManagement.addContact(contact6);
+        contactManagement.addContact(contact7);
+        contactManagement.addContact(contact8);
 
         contactManagement.removeContact("0734567890");
 
-        List<Contact> foundContacts = contactManagement.getContacts();
+        int foundContacts = contactManagement.showContacts();
 
         List<Contact> addedContacts = new ArrayList<>();
-        addedContacts.add(contact1);
-        addedContacts.add(contact2);
-        addedContacts.add(contact3);
+        addedContacts.add(contact6);
+        addedContacts.add(contact7);
+        addedContacts.add(contact8);
 
-        assertEquals(2, foundContacts.size());
-        assertNotEquals(3, foundContacts.size());
+        assertEquals(2, foundContacts);
+        assertNotEquals(3, foundContacts);
         assertNotEquals(addedContacts, foundContacts);
 
     }
