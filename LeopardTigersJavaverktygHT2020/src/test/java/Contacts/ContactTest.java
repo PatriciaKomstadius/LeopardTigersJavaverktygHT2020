a511939a089437b2116cda933030d84a103fe16b
Johan Astrom <johan.astrom89@gmail.com>
2020-12-10 21:26:33 +0100

    Tog bort junit 4 dependency i POM

--- LeopardTigersJavaverktygHT2020/src/test/java/Contacts/ContactTest.java
+++ LeopardTigersJavaverktygHT2020/src/test/java/Contacts/ContactTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.Assert.assertNotNull;
 
 public class ContactTest {
 
