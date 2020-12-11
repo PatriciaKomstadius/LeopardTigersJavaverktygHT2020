241081aac2dad5f20f357e85ac644b8bd3dc98fa
Patricia Komstadius <patricia.komstadius@gmail.com>
2020-12-11 21:52:18 +0100

    fix med add-metodens felhantering och test UI-klassen

--- LeopardTigersJavaverktygHT2020/src/test/java/UI/UserInputTest.java
+++ LeopardTigersJavaverktygHT2020/src/test/java/UI/UserInputTest.java
@@ -3,22 +3,18 @@ package UI;
 import Contacts.ContactManagement;
 import Contacts.Contact;
 
-
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static Contacts.ContactManagement.search;
 import static Contacts.ContactManagement.showContacts;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
-
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserInputTest {
 
@@ -30,28 +26,18 @@ public class UserInputTest {
     @DisplayName("Adding contact")
     @Test
     void testAdd() {
-        Contact contact = new Contact("Elena", "Mandela", "12345");
+        Contact contact = new Contact("Elena", "Mandela", "0105588");
 
         ContactManagement.addContact(contact);
 
         assertEquals(1, ContactManagement.showContacts());
     }
 
-/*
-    @DisplayName("Removing contact from contactbook")
-    @Test
-    void testDelete() {
-        String deleteContact1 = "12345";
-        ContactManagement.removeContact(deleteContact1);
-        assertEquals(0, ContactManagement.showContacts());
-    }
-*/
-
     @DisplayName("Deleting contact, checking contactlist")
     @Test
     void testSearchAndDelete() {
 
-        Contact testContact = new Contact("Evert", "Gustavsson", "008888");
+        Contact testContact = new Contact("Evert", "Gustavsson", "0708899245");
 
         ContactManagement.addContact(testContact);
         ContactManagement.removeContact(testContact.getNumber(), true);
@@ -60,39 +46,22 @@ public class UserInputTest {
 
     }
 
+    @DisplayName("Searching for contacts")
     @Test
     void testConsoleSearch() {
-        Contact testContact = new Contact("Ebba", "Grön", "800");
+        Contact testContact = new Contact("Ebba", "Grön", "0812345678");
+        ContactManagement.addContact(testContact);
 
         String value = "Ebba";
 
-         List<Contact> result = search(value);
+        List<Contact> testResult = new ArrayList<>();
+        testResult.add(testContact);
 
-//        assertTrue(testContact.getFirstName().equals("Ebba"));
-   assertEquals(value, ContactManagement.search(value));
-
+        assertEquals(testResult, ContactManagement.search(value));
     }
 
-    /*
-    @DisplayName("Searching by first name")
-    @Test
-    void testSearchByFirstName() {
-        Contact contact = new Contact("Femman", "Johansson", "5555");
 
-        assertEquals("Femman", contact.getFirstName());
-    }
-
-    @DisplayName("Searching by last name")
-    @Test
-    void testSearchByLastName() {
-        Contact contact = new Contact("Johan", "Fyrson", "4444");
-
-        assertEquals("Fyrson", contact.getLastName());
-    }
-
-     */
-
-    @DisplayName("Verifying throws of InputMisMatchException menu-choices")
+    @DisplayName("Verifying throws of InputMisMatchException in menu")
     @Test
     void testReadInt() {
         UserInput ui = new UserInput();
@@ -124,18 +93,30 @@ public class UserInputTest {
 
 
     @Test
-    void verifyingAdd() {
-
+    void testAddThrows() {
         UserInput ui = new UserInput();
 
-        Contact input = new Contact("Ebba", "Grön", "4004");
+        String input = "5";
 
-
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
 
         assertThrows(Exception.class, () -> ui.add());
 
-
     }
+
+    @Disabled
+    @Test
+    void verifyingAdd() {
+
+        UserInput ui = new UserInput();
+        Contact input = new Contact("Ebba", "Grön", "4004");
+
+        String number = "5";
+
+        assertThrows(Exception.class, () -> ui.add());
+    }
+
 
     @Disabled
     @Test
@@ -152,6 +133,7 @@ public class UserInputTest {
         assertThrows(Exception.class, () -> ui.add());
 
     }
+
 
 }
 
